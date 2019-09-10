922863555321a2c0678ceab349f1da5f44b7b529
Roy Kim <roykim@roykim-mac.riotgames.com>
2019-09-10 18:05:47 +0900

    0911

--- scan.go
+++ scan.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
-//	"strings"
+	"os/exec"
 )
 
 func main() {
-	line:="abcde"
-	
-	
-	//a:=strings.Index(line, "f")
-	fmt.Println(line[3:len(line)])
+
+	getSarInfo()
+
+}
+
+func getSarInfo() error {
+	IP_ADDR := "10.80.51.232"
+	//command := fmt.Sprintf("ssh %s sar 1 10", IP_ADDR)
+	//fmt.Println(command)
+	out, _ := exec.Command("ssh", IP_ADDR, "sar", "1", "5").Output()
+	output := string(out[:])
+
+	fmt.Println(output)
+
+	return nil
 }
